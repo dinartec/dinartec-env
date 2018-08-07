08e05f0500b1a5044358469ab28699fb1627a4d4
dinartec <dinartec@users.noreply.github.com>
2018-08-06 21:56:26 -0500

    Add webpack to project

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import numeral from 'numeral';
+
+numeral.register('locale', 'ni', {
+    delimiters: {
+        thousands: ',',
+        decimal: '.'
+    },
+    abbreviations: {
+        thousand: 'k',
+        million: 'm',
+        billion: 'b',
+        trillion: 't'
+    },
+    ordinal : function (number) {
+        return 'o';
+    },
+    currency: {
+        symbol: 'C$'
+    }
+});
+
+// switch between locales
+numeral.locale('ni');
+
+const newValue = numeral(1000).format('$0,0.0');
+console.log(`In cordobas, the new value is ${newValue}`);
